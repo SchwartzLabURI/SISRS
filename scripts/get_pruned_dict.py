b5f210a40f9b1ee9d760aa0e48ae45d2d66961b3
BobLiterman <jazz710@gmail.com>
2019-03-29 11:38:30 -0400

    Updated get_pruned_dict.py for new input.

--- scripts/get_pruned_dict.py
+++ scripts/get_pruned_dict.py
@@ -3,44 +3,44 @@
 """Take multiple mpileup files and determine the base at each position of the reference genome
 
     arguments:
-        path: folder containing mpileup files ending in pileups
-        contig_dir: folder containing assembled composite genome
-        minread: number of reads at a position required to call a base
-        thresh: proportion of reads that must be one base for calling to occur
+        tax_dir: folder containing mpileup files ending in pileups (SISRS_Run/SPP)
+        contig_dir: folder containing assembled composite genome (SISRS_Run/Composite_Genome)
+        minread: number of reads at a position required to call a base (3 is a good start)
+        thresh: proportion of reads that must be one base for calling to occur (1 means no intraspecies variation)
 
     output:
-        path/<SP>_LocList: list of bases for each position
+        tax_dir/<SP>_LocList: list of bases for each position
 """
 
 import sys
 from collections import Counter
-import glob
+from glob import glob
 import string
 import re
 import os
-from specific_genome import getCleanList
+from os import path
 from collections import defaultdict
 
 #get combined pileup info
-def getallbases(path,contig_dir,minread,thresh):
-    basePath=os.path.dirname(path)
-    assert len(glob.glob1(path,"*.pileups"))==1,'More than one pileup file in'+path
+def getallbases(tax_dir,contig_dir,minread,thresh):
+    basePath=path.dirname(tax_dir)
+    assert len(glob(tax_dir+"/*.pileups"))==1,'More than one pileup file in'+tax_dir
     speciesDict=defaultdict(lambda: 'N')
     minPenalty=0
     threshPenalty=0
     bothPenalty=0
-    with open (path+'/'+os.path.basename(path)+'.pileups',"r") as filein:
+    with open (tax_dir+"/"+path.basename(tax_dir)+'.pileups',"r") as filein:
         for line in iter(filein):
             splitline=line.split()
-            if len(splitline)>4:
+            if len(splitline)>4 & int(splitline[3])>0:
                 node,pos,ref,num,bases,qual=line.split()
                 loc=node+'/'+pos
                 cleanBases=getCleanList(ref,bases)  #Get clean bases where * replaced with -
                 finalBase,minPenalty,threshPenalty,bothPenalty=getFinalBase_Pruned(cleanBases,minread,thresh,minPenalty,threshPenalty,bothPenalty)
                 speciesDict[loc] = finalBase
 
-    printSpecies = open(path+"/"+os.path.basename(path)+'_LocList', 'w')
-    with open(contig_dir+"/contigs_LocList") as f:
+    printSpecies = open(tax_dir+"/"+path.basename(tax_dir)+'_LocList', 'w')
+    with open(contig_dir+"/Composite_Genome_LocList") as f:
         for line in f:
             printSpecies.write(speciesDict[line.strip()]+'\n')
     f.close()
@@ -51,10 +51,41 @@ def getallbases(path,contig_dir,minread,thresh):
     siteCount = len(speciesDict) - nCount
     sitePercent = format((float(siteCount)/len(speciesDict))*100,'.2f')
     nPercent = format((float(nCount)/len(speciesDict))*100,'.2f')
-    print("Of "+ str(len(speciesDict)) + " positions, " + os.path.basename(path) + " has good calls for " + str(siteCount) + " sites (" + sitePercent +"%). There were " + str(nCount) + " N calls ("+ nPercent + "%).",flush=True)
-    print("Of " + str(nCount) + " Ns, " + os.path.basename(path) + " lost " + str(threshPenalty) + " via homozygosity threshold, " + str(minPenalty)  +" from low coverage, and " + str(bothPenalty) + " from both. "+ str(nCount - threshPenalty - minPenalty - bothPenalty) + " sites had no pileup data.\n",flush=True)
+    print("Of "+ str(len(speciesDict)) + " positions, " + path.basename(tax_dir) + " has good calls for " + str(siteCount) + " sites (" + sitePercent +"%). There were " + str(nCount) + " N calls ("+ nPercent + "%).",flush=True)
+    print("Of " + str(nCount) + " Ns, " + path.basename(tax_dir) + " lost " + str(threshPenalty) + " via homozygosity threshold, " + str(minPenalty)  +" from low coverage, and " + str(bothPenalty) + " from both. "+ str(nCount - threshPenalty - minPenalty - bothPenalty) + " sites had no pileup data.\n",flush=True)
 
     return siteCount
+
+def getCleanList(ref,bases):
+    bases=bases.replace('.',ref) #insert ref base
+    bases=bases.replace(',',ref)
+    bases=bases.upper() #everything in uppercase
+    bases=list(bases)
+
+    okbases=['A','C','G','T','*']
+    indels=['+','-']
+
+    new_base_list=[]
+    ibase_list = iter(bases)
+    for b in ibase_list:
+        if b in okbases:
+            new_base_list.append(b)         #Get base
+        elif b in indels:                   #skip indels
+            i = int(next(ibase_list))
+            j = str(next(ibase_list))
+            if str.isdigit(j):
+                skip=int(str(i)+j)
+                while skip>0:
+                    z=next(ibase_list)
+                    skip=skip-1
+            else:
+                while i>1:
+                    z=next(ibase_list)
+                    i = i-1
+        elif b=='^':                        #skip read qual noted at end of read
+            z=next(ibase_list)
+
+    return new_base_list
 
 def getFinalBase_Pruned(cleanBases,minread,thresh,minPenalty,threshPenalty,bothPenalty):
     singleBase=(Counter(cleanBases).most_common()[0][0])
@@ -75,16 +106,16 @@ def getFinalBase_Pruned(cleanBases,minread,thresh,minPenalty,threshPenalty,bothP
 
     return finalBase,minPenalty,threshPenalty,bothPenalty
 ###############################################
-def main(path, contig_dir, minread, thresh):
+def main(tax_dir, contig_dir, minread, thresh):
 
-    allbases=getallbases(path,contig_dir,minread,thresh)      #dictionary of combined pileups - locus/pos:bases(as list)
+    allbases=getallbases(tax_dir,contig_dir,minread,thresh)      #dictionary of combined pileups - locus/pos:bases(as list)
     if allbases==0:
-        print('No data for '+path,flush=True)
+        print('No data for '+tax_dir,flush=True)
         sys.exit(1)
 
 if __name__ == '__main__':
-    path = sys.argv[1]
+    tax_dir = sys.argv[1]
     contig_dir = sys.argv[2]
     minread = int(sys.argv[3])
     thresh = float(sys.argv[4])
-    main(path, contig_dir, minread, thresh)
+    main(tax_dir, contig_dir, minread, thresh)
