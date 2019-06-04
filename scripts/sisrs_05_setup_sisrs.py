6a0d20db4609773fc66b9f3a1ac1fe53fa117343
Devin McConnell <devinmcconnell@199.128.20.172.s.wireless.uri.edu>
2019-06-04 11:07:05 -0400

    Fixed a bug in sisrs_05 and finished commenting.

--- scripts/sisrs_05_setup_sisrs.py
+++ scripts/sisrs_05_setup_sisrs.py
@@ -11,6 +11,11 @@ import sys
 from glob import glob
 import pandas as pd
 
+'''
+This function is designed to obtain all of the directories needed to finish the
+setup of sisrs. This function only needs the working sisrs directory as a
+parameter.
+'''
 def obtainDir(outPath):
 
     #Set TrimRead + SISRS directories based off of script folder location
@@ -30,6 +35,11 @@ def obtainDir(outPath):
 
     return trim_read_tax_dirs,ray_dir,sisrs_dir,composite_dir
 
+'''
+This function is designed to raname the contig files and make copies of the
+length files that are a product of the output from Ray. It requieres as input
+the directory were ray output all of its dated and the composite directory.
+'''
 def fileChanges(ray_dir,composite_dir):
 
     #Create renamed contig file in SISRS_Run/Composite_Genome
@@ -48,6 +58,12 @@ def fileChanges(ray_dir,composite_dir):
     contig_length.Contig = ("SISRS_" + contig_length.Contig)
     contig_length.to_csv(composite_dir+"/contigs_SeqLength.tsv",sep="\t",header=None,index=False)
 
+'''
+This function is designed to call the bowtie comman and the samtools command
+in order to index the composite genomes correctly. This function requiers two
+parameters the composite genome directory and the number of threads that it can
+run with
+'''
 def indexCompGenome(composite_dir,threads):
     #Index composite genome using Bowtie2 and Samtools
     bowtie_command = [
@@ -64,6 +80,11 @@ def indexCompGenome(composite_dir,threads):
         '{}'.format(composite_dir+'/contigs.fa')]
     os.system(" ".join(bowtie_command))
 
+'''
+This function is designed to start the file setups requiered for sisrs to run
+properly. It requires as parameters the composite_dir and the location of were
+all of the scripts are located
+'''
 def beginSetUp(composite_dir,script_dir):
     #Create file with an entry for each individual site in the alignment
     siteCount=0
@@ -78,6 +99,7 @@ def beginSetUp(composite_dir,script_dir):
 
     print("==== Site list created: " + str(siteCount) + " total sites ==== \n",flush=True)
 
+    # Read in the bash script
     sisrs_template = ""
 
     shFile = open(script_dir + '/sisrs_05_template.sh')
@@ -86,14 +108,22 @@ def beginSetUp(composite_dir,script_dir):
 
     return sisrs_template
 
+
+'''
+This function is designed to complete the sisrs setup and will be going through
+to replace all of the non-specfic portions of the bash script with more specfic
+portions of the scripts. This function requiers all of the directory information
+alond with the number of threads, mindread, and threshold.
+'''
 def copyShFile(trim_read_tax_dirs,sisrs_dir,sisrs_template,composite_dir,outPath,threads,minread,threshold):
 
+    composite_dir = os.path.abspath(composite_dir)
+    sisrs_dir = os.path.abspath(sisrs_dir)
     #Create links to trimmed read files in SISRS_Run directory
     for tax_dir in trim_read_tax_dirs:
         tax_dir = os.path.abspath(tax_dir)
         print(tax_dir)
         taxa = path.basename(tax_dir[:])
-        print(taxa)
         read_link_command = [
             'cd',
             '{}'.format(sisrs_dir+"/"+taxa),
@@ -102,7 +132,6 @@ def copyShFile(trim_read_tax_dirs,sisrs_dir,sisrs_template,composite_dir,outPath
             '-as',
             '{}/*.fastq.gz'.format(tax_dir),
             '.']
-        print(read_link_command)
         os.system(" ".join(read_link_command))
 
         new_sisrs = sisrs_template
@@ -117,8 +146,9 @@ def copyShFile(trim_read_tax_dirs,sisrs_dir,sisrs_template,composite_dir,outPath
                    'TAXA':taxa,
                    'SISRS_DIR':sisrs_dir,
                    'COMPOSITE_DIR':composite_dir,
-                   'READS':",".join(glob(tax_dir+"*.fastq.gz"))}
+                   'READS':",".join(glob(tax_dir+"/*.fastq.gz"))}
         for key in keyList:
+            print(key,keyDict[key])
             new_sisrs = new_sisrs.replace(key,keyDict[key])
         with open(sisrs_dir+"/"+taxa+"/"+taxa+".sh", "w") as text_file:
             print(new_sisrs, file=text_file)
