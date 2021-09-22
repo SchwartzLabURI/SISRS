6f6427fadf0e12e01e4979d6f2ebe9aa29ab47a1
Rachel Schwartz <rsschwartz@gmail.com>
2021-09-22 16:59:14 -0400

    add svd quartets script

---
+++ scripts/slurm_submissions/runSVDQ.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name="svdq"
+#SBATCH --time=150:00:00  # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=36   # CHANGE processor core(s) per node
+#SBATCH --mail-user=your_email #CHANGE to your email
+
+cd $SLURM_SUBMIT_DIR
+module load PAUP/4.0a168-centos64
+
+alignmentFile="svdquartets_tutorial/anomaly_zone.nex" #Change to the path to your phylip file
+logFile="${alignmentFile}.svdq.log"
+treeFile="${alignmentFile}.svdq.tre"
+
+nBoot=1000
+nCpus=36
+
+date
+
+echo "
+log file=${logFile};
+tonexus format=RelPHYLIP fromfile=${alignmentFile} tofile=${alignmentFile}.svdq.nex interleaved=yes replace=yes;
+exe ${alignmentFile}.svdq.nex;
+svdq bootstrap nreps=${nBoot} nthreads=${nCpus};
+saveTrees file=${treeFile};
+quit;
+" | paup
+
+date
