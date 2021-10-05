a9b96095314478a4dfcb6a2cfd4babd09ac5d802
Rachel Schwartz <rsschwartz@gmail.com>
2021-10-05 11:44:41 -0400

    update raxml script with instructions

--- scripts/slurm_submissions/runRAxML.sh
+++ scripts/slurm_submissions/runRAxML.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name="RAxML"
 #SBATCH --time=96:00:00  # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --ntasks-per-node=20   # processor core(s) per node 
-#SBATCH --mail-user="user@example.edu"
+#SBATCH --ntasks-per-node=20   # CHANGE processor core(s) per node 
+#SBATCH --mail-user="user@example.edu" #CHANGE to your email
 #SBATCH --mail-type=END,FAIL
 
 cd $SLURM_SUBMIT_DIR
@@ -12,4 +12,8 @@ module purge
 
 module load RAxML/8.2.12-intel-2019b-hybrid-avx2
 
-raxmlHPC -s <path to alignment file> -n <name of output file> -m ASC_GTRGAMMA --asc-corr="lewis" -T 20 -f a -p $RANDOM -N 100 -x $RANDOM
+ALIGNMENT=  #Put the path to your alignment file here
+OUTPUT=  #Give your output file a name relevant to the alignment
+P=20 #Change to the number of processors (20 or 36)
+
+raxmlHPC -s $ALIGNMENT -n $OUTPUT -m ASC_GTRGAMMA --asc-corr="lewis" -T $P -f a -p $RANDOM -N 100 -x $RANDOM
