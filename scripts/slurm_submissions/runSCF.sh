5858ab645f13c93fe6fe1bd8e7907ed8637cc55e
rachelss <rachelss@users.noreply.github.com>
2021-10-28 21:23:01 -0400

    update for iqtree2

--- scripts/slurm_submissions/runSCF.sh
+++ scripts/slurm_submissions/runSCF.sh
@@ -12,12 +12,12 @@
 
 cd $SLURM_SUBMIT_DIR
 
-module load IQ-TREE/1.7-beta16-foss-2018b
+module load IQ-TREE/2.1.2-foss-2020a
 
 TREE=RAxML_bipartitionsBranchLabels.M2_nogap_clean #CHANGE to your filename (include the path as needed)
 ALIGNMENT=alignment_pi_m2_nogap_clean.phylip-relaxed #CHANGE to the file used to generate the tree
 OUTPUT=ALGN1_SCF #CHANGE to a name that will indicate the output information relevant to this tree
 T=20 #CHANGE to the number of processors (20 or 36)
 
-iqtree -t $TREE -s $ALIGNMENT --scf 100 --prefix ALGN1_SCF -nt $T
+iqtree2 -t $TREE -s $ALIGNMENT --scf 100 --prefix ALGN1_SCF -nt $T
 
