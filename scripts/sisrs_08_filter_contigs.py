4bbb3be7dc60006c0050e3938c1f5bc40d4da30b
Rachel Schwartz <rsschwartz@gmail.com>
2022-08-11 16:19:02 -0400

    add scripts for probe development

--- scripts/sisrs_08_filter_contigs.py
+++ scripts/sisrs_08_filter_contigs.py
@@ -1,4 +1,9 @@
-import sys
+#!/usr/bin/env python3
+
+import csv
+from collections import Counter
+from Bio import SeqIO
+from os import path, mkdir, listdir, system
 
 #get file paths to alignment_pi_locs_m25.txt and contigs_SeqLength.tsv
 #path_to_seq_lengths: Path to .tsv where contig lengths are located.
@@ -11,42 +16,118 @@ import sys
 #path_to_output: Path to write filtered_data.tsv to. Must include the final backslash.
 #                 Ex: "C://Users/caleb/OneDrive/Desktop/"
 
-
-path_to_seq_lengths, path_to_sites, min_threshold = sys.argv[1], sys.argv[2], sys.argv[3]
-
-#Example:
-#path_to_seq_lengths, path_to_sites, min_threshold, path_to_output = "C://Users/caleb/OneDrive/Desktop/contigs_SeqLength2.tsv", "C://Users/caleb/OneDrive/Desktop/alignment_pi_locs_m25.txt", 0.1, "C://Users/caleb/OneDrive/Desktop/"
-
-#path_to_sites = "C://Users/caleb/OneDrive/Desktop/alignment_pi_locs_m25.txt"
-#path_to_seq_lengths = "C://Users/caleb/OneDrive/Desktop/contigs_SeqLength.tsv.txt.txt"
+outPath = sys.argv[1]
+#outPath = '../../SISRS_Small_test'
+min_threshold = int(sys.argv[3]) #number of variable sites (not as described above)
+#min_threshold = int(0)
+path_to_seq_lengths = outPath + '/SISRS_Run/Composite_Genome/contigs_SeqLength.tsv'
+path_to_sites = outPath + '/SISRS_Run/' + sys.argv[2] #name of file eg alignment_pi_locs_m2.txt - use something minimally missing
+#path_to_sites = outPath + '/SISRS_Run/alignment_pi_locs_m2.txt'
+path_to_output = outPath + '/SISRS_Run/'
 
 #read in informative site names
 with open(path_to_sites) as file:
-   sites = file.readlines()
+    sites = dict(csv.reader(file, delimiter='/')) #SISRS_contig-2/44  
 
 #read in contig names and their overall lengths
-with open(path_to_seq_lengths) as file:
-    contigs = file.readlines()
+with open(path_to_seq_lengths, newline = '') as file:
+    contigs = dict(csv.reader(file, delimiter='\t')) #SISRS_contig-2    351
+
+contigcounts = dict(Counter(sites.keys())) #number of variable sites
+high_count_contigs = [k for k, v in contigcounts.items() if int(v) > min_threshold and int(contigs[k]) > 160]
+
+composite = SeqIO.to_dict(SeqIO.parse(outPath + "/SISRS_Run/Composite_Genome/contigs.fa", "fasta"))     #>SISRS_contig-2000001 186 nucleotides
+
+with open(outPath+'/TaxonList.txt') as f:
+    taxon_list = f.readlines()
+    taxon_list = sorted([x.rstrip() for x in taxon_list])
+
+num_sp = len(taxon_list)
+
+#see https://onlinelibrary.wiley.com/doi/full/10.1002/ece3.5260
+
+#goals:
+#1. at least 160 bp = 2 probes
+#2. each sp no more than 7% different from the composite
+#3. 60k probes total = 4m bp
+#all sp in contig alignment
+#less than X% missing per site - info for each spp
+
+
+good_contigs = []
+
+new_contig_folder = outPath + "/SISRS_Run/contigs_outputs2/"
+if not path.exists(new_contig_folder):
+    mkdir(new_contig_folder)
+    
+newnew_contig_folder = outPath + "/SISRS_Run/aligned_contigs2/"
+if not path.exists(newnew_contig_folder):
+    mkdir(newnew_contig_folder)
+        
+for k in high_count_contigs:
+    a_file = outPath + '/SISRS_Run/contigs_outputs/' + k + '.fasta'
+    if path.exists(a_file):
+        alignment = list(SeqIO.parse(a_file, "fasta"))
+        if len(alignment) == num_sp: #check we have all spp
+            composite_seq = composite[k] #get composite seq for this contig
+            alignment.append(composite_seq)
+            SeqIO.write(alignment, new_contig_folder + k + '.fasta', "fasta")
+            
+# list of contigs
+contig_list = listdir(new_contig_folder)
+for con in contig_list:
+    con_p_in = path.join(new_contig_folder, con)
+    con_p_out = path.join(newnew_contig_folder, con)
+    mafft_command = f"mafft --auto --thread {thr} {con_p_in} > {con_p_out}"
+    system(mafft_command)
+
+for k in high_count_contigs:
+    distances = {}    
+    a_file = newnew_contig_folder + k + '.fasta'
+    if path.exists(a_file):
+        alignment = SeqIO.to_dict(SeqIO.parse(a_file, "fasta"))
+        composite_seq = composite[k]
+        
+        for sp, seq in alignment.items():
+            if sp in taxon_list:
+                seq2 = str(seq.seq).upper()
+                #print(seq2)
+                #print(str(composite_seq.seq))
+                distances[sp] = 0
+                for i in range(len(seq2)): #go through seq site by site to get distance
+                    if seq2[i] != '-':
+                        if composite_seq[i] != seq2[i]:
+                            distances[sp]+=1
+                distances[sp] = distances[sp] / len(seq2.replace('-',''))
+                #print(distances[sp])
+        if max(distances.values()) < 0.08:
+            good_contigs.append(k)
+        else:
+            print(sorted(distances.values()))
+
+
+with open(outPath + "/SISRS_Run/contigs_for_probes.txt", 'w') as f:
+    for line in good_contigss:
+        f.write(f"{line}\n")
 
 
 #This function works similar to grep in R or bash, but it searches for an input string through
 #a list rather than a file/directory. Strings in the list 'stringList' that contain the substring 'searchString'
 #will be returned in list format.
 
-grep = lambda searchString, stringList: [j for j in stringList if j.__contains__(searchString)]
+#grep = lambda searchString, stringList: [j for j in stringList if j.__contains__(searchString)]
+
+#contig_names = [i.split(",")[0] for i in contigs]
+#info_sites = [len((grep(i, sites))) for i in contig_names][2:]
+#possible_sites = [i.split(",")[1] for i in contigs]
+#possible_sites = [int(i) for i in possible_sites[1:]]
+
+#min_threshold = float(min_threshold)
 
 
-contig_names = [i.split(",")[0] for i in contigs]
-info_sites = [len((grep(i, sites))) for i in contig_names][2:]
-possible_sites = [i.split(",")[1] for i in contigs]
-possible_sites = [int(i) for i in possible_sites[1:]]
-
-min_threshold = float(min_threshold)
-
-
-for i in range(len(info_sites)):
-    if(float(info_sites[i]) / float(possible_sites[i]) >= min_threshold):
-        print(contig_names[i] + "   " + str(float(info_sites[i]) / float(possible_sites[i])))
+#for i in range(len(info_sites)):
+#    if(float(info_sites[i]) / float(possible_sites[i]) >= min_threshold):
+#        print(contig_names[i] + "   " + str(float(info_sites[i]) / float(possible_sites[i])))
 
 
 
