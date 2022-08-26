34819740ea34fe9040d9406b04015a45f83f2500
Rachel Schwartz <rsschwartz@gmail.com>
2022-08-26 16:19:28 -0400

    filter based on het (paralogs?)

--- scripts/sisrs_08b_similarilty.py
+++ scripts/sisrs_08b_similarilty.py
@@ -32,6 +32,32 @@ with open(matrix_file, newline = '') as file: #qseqid sseqid qlen slen length pi
 loci_to_drop = list(set(loci_to_drop))
 print(loci_to_drop)
 
+# list of taxa
+with open(outpath+'/TaxonList.txt') as f:
+    taxonlist = f.readlines()
+    taxonlist = sorted([x.rstrip() for x in taxonlist])
+
+hz_dict = {} #keep track of which contigs have heterozygosity (= paralogs?)
+for taxon in taxonlist:
+    f = outpath + 'SISRS_Run/' + taxon + '/' + taxon + '_hztable.csv' #het table output by step 7
+    with open(f, newline = '') as fi:
+        hz_table = csv.reader(fi, delimiter=',') #iterator: SISRS_contig-5000011,0.09424083769633508
+        for line in hz_table:
+            #print(line)
+            if line[1] != 'NA': #seq not found in sp
+                if float(line[1]) > 0.01: #high het
+                    if line[0] in hz_dict:
+                        hz_dict[line[0]].append(taxon)
+                    else:
+                        hz_dict[line[0]] = [taxon]
+
+for k, v in hz_dict.items():
+    if len(v) > 0.5 * len(taxonlist): #half taxon list may have paralogs - this is arbitrary
+        loci_to_drop.append(k)
+
+loci_to_drop = list(set(loci_to_drop))
+print(loci_to_drop)
+
 #read fasta and remove loci
 contig_seqs = []
 with open(fasta) as handle:
