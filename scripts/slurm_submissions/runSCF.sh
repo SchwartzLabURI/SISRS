f10bc142a22bcaeb70ab861e165497acfa75210d
rachelss <rachelss@users.noreply.github.com>
2022-10-13 21:18:56 -0400

    longer path to alignment

--- scripts/slurm_submissions/runSCF.sh
+++ scripts/slurm_submissions/runSCF.sh
@@ -15,7 +15,7 @@ cd $SLURM_SUBMIT_DIR
 module load IQ-TREE/2.1.2-foss-2020a
 
 TREE=RAxML_bestTree.alignment_pi_m3_nogap #CHANGE to your filename (include the path as needed)
-ALIGNMENT=alignment_pi_m3_nogap.phylip-relaxed #CHANGE to the file used to generate the tree
+ALIGNMENT=../../SISRS_Small_test/SISRS_Run/alignment_pi_m3_nogap.phylip-relaxed #CHANGE to the file used to generate the tree
 OUTPUT=alignment_pi_m3_nogap #CHANGE to a name that will indicate the output information relevant to this tree
 T=36 #CHANGE to the number of processors (20 or 36)
 
