9ceee441b62cb7983e84177110885d42b1113e0a
rachelss <rachelss@users.noreply.github.com>
2022-12-03 15:59:30 -0500

    make default output name

--- scripts/slurm_submissions/runRAxML.sh
+++ scripts/slurm_submissions/runRAxML.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name="RAxML"
 #SBATCH --time=96:00:00  # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --ntasks-per-node=20   # CHANGE processor core(s) per node 
+#SBATCH --ntasks-per-node=36   # CHANGE processor core(s) per node 
 #SBATCH --mail-user="user@example.edu" #CHANGE to your email
 #SBATCH --mail-type=END,FAIL
 
@@ -12,8 +12,8 @@ module purge
 
 module load RAxML/8.2.12-intel-2019b-hybrid-avx2
 
-ALIGNMENT=  #Put the path to your alignment file here
-OUTPUT=  #Give your output file a name relevant to the alignment
-P=20 #Change to the number of processors (20 or 36)
+ALIGNMENT=~/SISRS_Small_test/alignment_pi_m1_nogap.phylip-relaxed  #Put the path to your alignment file here
+OUTPUT=$(basename ${ALIGNMENT%.*})  #output filename relevant to the alignment
+P=36 #Change to the number of processors (20 or 36)
 
 raxmlHPC -s $ALIGNMENT -n $OUTPUT -m ASC_GTRGAMMA --asc-corr="lewis" -T $P -f a -p $RANDOM -N 100 -x $RANDOM
