1760983c40a28b01481349fffcba00ed31e77564
Rachel Schwartz <rsschwartz@gmail.com>
2024-06-05 13:50:32 -0400

    add a test

--- scripts/test_sisrs_08_filter_contigs.py
+++ scripts/test_sisrs_08_filter_contigs.py
@@ -13,22 +13,14 @@ def setup_data():
     min_threshold = 1
     length_of_locus = 1000
 
+    taxon_list = get_taxon_list('../test_data/TaxonList.txt')
+
     composite = SeqIO.to_dict(SeqIO.parse("../test_data/contigs.fa", "fasta"))
 
     high_count_contigs, contigcounts = get_high_count_contigs(path_to_seq_lengths, path_to_sites, min_threshold, length_of_locus)
 
-    return(high_count_contigs, contigcounts, composite)
-
-
-def test_get_high_count_contigs_basic(setup_data):
-    assert len(setup_data[0]) == 2
-
-def test_write_alignment_plus_composite_basic(setup_data):
-    high_count_contigs = setup_data[0]
     contigPath = '../test_data/'
     num_sp = 4
-    composite = setup_data[2]
-
     new_contig_folder = '../test_data/temp/'
     if not path.exists(new_contig_folder):
         os.mkdir(new_contig_folder)
@@ -37,6 +29,23 @@ def test_write_alignment_plus_composite_basic(setup_data):
         os.mkdir(new_contig_folder)
 
     contigs_remaining = write_alignment_plus_composite(high_count_contigs, contigPath, num_sp, composite, new_contig_folder)
-    assert contigs_remaining == 2
 
-    shutil.rmtree(new_contig_folder)
+    return(high_count_contigs, contigcounts, composite, taxon_list, contigs_remaining)
+
+
+def test_get_high_count_contigs_basic(setup_data):
+    assert len(setup_data[0]) == 2
+
+def test_write_alignment_plus_composite_basic(setup_data):
+
+    assert setup_data[4] == 2
+
+def test_filter_contigs_distance_basic(setup_data):
+
+    a = '../test_data/aligned/'
+
+    contigs_remaining08 = filter_contigs_distance(setup_data[0], a, setup_data[3], 0.08, setup_data[1])
+    contigs_remaining11 = filter_contigs_distance(setup_data[0], a, setup_data[3], 0.11, setup_data[1])
+
+    assert len(contigs_remaining08) == 0
+    assert len(contigs_remaining11) == 2
