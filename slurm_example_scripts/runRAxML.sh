aba3692811cae772c7fff0c001436d402fdc4d87
Rachel Schwartz <rsschwartz@gmail.com>
2024-10-01 11:08:02 -0400

    edit for unity

--- slurm_example_scripts/runRAxML.sh
+++ slurm_example_scripts/runRAxML.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 #SBATCH --job-name="RAxML"
-#SBATCH --time=96:00:00  # walltime limit (HH:MM:SS)
+#SBATCH --time=6:00:00  # CHANGE - walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --ntasks-per-node=36   # CHANGE processor core(s) per node 
-#SBATCH --mail-user="user@example.edu" #CHANGE to your email
+#SBATCH --ntasks-per-node=20   # CHANGE processor core(s) per node 
+#SBATCH --mail-user="rsschwartz@uri.edu" #CHANGE to your email
 #SBATCH --mail-type=ALL
 #SBATCH --output=slurm_%x_%j.out
 #SBATCH --error=slurm_%x_%j.err
+#SBATCH -p uri-cpu
 
 cd $SLURM_SUBMIT_DIR
 
 module purge
 
-module load RAxML/8.2.12-intel-2019b-hybrid-avx2
+#CHANGE IF NOT ON UNITY
+module load intel-oneapi-mpi/2021.6.0 apptainer/latest shpc/0.1.26 raxml/8.2.12
 
-ALIGNMENT=~/SISRS_Small_test/alignment_pi_m1_nogap.phylip-relaxed  #Put the path to your alignment file here
+ALIGNMENT=../../SISRS_Small_test/SISRS_Run/alignment_pi_m1_nogap.phylip-relaxed  #CHANGE - Put the path to your alignment file here
+
 OUTPUT=$(basename ${ALIGNMENT%.*})  #output filename relevant to the alignment
-P=36 #Change to the number of processors (20 or 36)
+P=$SLURM_JOB_CPUS_PER_NODE
 
 raxmlHPC -s $ALIGNMENT -n $OUTPUT -m ASC_GTRGAMMA --asc-corr="lewis" -T $P -f a -p $RANDOM -N 100 -x $RANDOM
--- slurm_example_scripts/runSCF.sh
+++ slurm_example_scripts/runSCF.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 #SBATCH --job-name="scfRun"
-#SBATCH --time=48:00:00  # walltime limit (HH:MM:SS)
+#SBATCH --time=4:00:00  # CHANGE walltime limit (HH:MM:SS)
 #SBATCH --nodes=1  # number of nodes
-#SBATCH --ntasks-per-node=36   # CHANGE to the number of processor core(s) per node
-#SBATCH --mail-user="user@example.edu" #CHANGE to your email address
+#SBATCH --ntasks-per-node=20   # CHANGE to the number of processor core(s) per node
+#SBATCH --mail-user="rsschwartz@uri.edu" #CHANGE to your email address
 #SBATCH --mail-type=ALL
 #SBATCH --output=slurm_%x_%j.out
 #SBATCH --error=slurm_%x_%j.err
+#SBATCH -p uri-cpu
 
-# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 
 cd $SLURM_SUBMIT_DIR
 
-module load IQ-TREE/2.2.2.3-gompi-2022a
+#CHANGE IF NOT ON UNITY
+module load iq-tree/2.1.3-noMPI
 
-TREE=RAxML_bestTree.alignment_pi_m3_nogap #CHANGE to your filename (include the path as needed)
-ALIGNMENT=../../SISRS_Small_test/SISRS_Run/alignment_pi_m3_nogap.phylip-relaxed #CHANGE to the file used to generate the tree
-OUTPUT=alignment_pi_m3_nogap #CHANGE to a name that will indicate the output information relevant to this tree
-T=36 #CHANGE to the number of processors (20 or 36)
+TREE=RAxML_bestTree.alignment_pi_m1_nogap #CHANGE to your filename (include the path as needed)
+ALIGNMENT=../../SISRS_Small_test/SISRS_Run/alignment_pi_m1_nogap.phylip-relaxed #CHANGE to the file used to generate the tree
+OUTPUT=alignment_pi_m1_nogap #CHANGE to a name that will indicate the output information relevant to this tree
+T=$SLURM_JOB_CPUS_PER_NODE
 
-iqtree2 -te $TREE -s $ALIGNMENT --scfl 100 --prefix $OUTPUT
+iqtree2 -te $TREE -s $ALIGNMENT --scf 100 --prefix $OUTPUT
 
-#compute gene trees
-ALN_DIR=
-iqtree2 -S $ALN_DIR --prefix loci -T $T # loci.treefile = a set of trees
-iqtree2 -t $TREE --gcf loci.treefile --prefix concord
 
